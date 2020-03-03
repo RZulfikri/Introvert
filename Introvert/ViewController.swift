aa703e40550d29b5bb32991daf8d02f502a46a6a
Rahmat Zulfikri <me@rahmatzulfikri.xyz>
2020-03-03 14:25:47 +0700

    Initial Commit

---
+++ Introvert/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Introvert
+//
+//  Created by Rahmat Zulfikri on 03/03/20.
+//  Copyright © 2020 Rahmat Zulfikri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
